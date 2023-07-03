ba2d377ff43f559e05401d03c5ccda1974a0466f
Kenneth luzolo <kenneth.luzolo@unitel.co.ao>
2023-07-04 00:46:00 +0100

    release filter  with criteria JPA

---
+++ src/main/java/it/nepsthermoney/repository/filter/ReleaseFilter.java
@@ -0,0 +1,28 @@
+package it.nepsthermoney.repository.filter;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReleaseFilter implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dueDateOf;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dueDateUntil;
+}
+
